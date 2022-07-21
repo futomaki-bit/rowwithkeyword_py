883851b994f04ff9800d9bea0d61835e2046bb60
futomakiyoin <72528988+futomaki-bit@users.noreply.github.com>
2022-07-20 22:34:40 -0400

    Initial upload

---
+++ main.py
@@ -0,0 +1,14 @@
+fi = open('text.txt','r')
+keywordfi = open('keywords.txt','r')
+keyrows = keywordfi.readlines()
+keywords = []
+for row in keyrows:
+    keywords.append(row)
+
+rows = fi.readlines()
+for row in rows:
+    for keyword in keywords:
+        if 'Note' not in row[:-1] and 'PS' not in row[:2]:
+            if keyword in row:
+                print(row[:-1])
+                break
